33ecb8fdfb9ee3f4f87ab92f7892e665f9772d4e
Alberto Campos <albertocamposhdez@hotmail.com>
2019-12-15 10:06:20 -0600

    Vistas de Login y Dashboard principal terminadas

---
+++ src/model/DbConnection.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author fields
+ */
+public class DbConnection {
+    private static final String USER ="root";
+    private static final String PASSWORD="";
+    private static final String DB ="sensores";
+    private static final String URL = "jdbc:mysql://localhost:3306/"+DB;
+    
+    public static Connection con = null;
+    
+    public static Connection connect(){
+        try{
+            con = (Connection) DriverManager.getConnection(URL ,USER, PASSWORD);
+            return con;
+        }catch(SQLException ex){
+            System.out.println(ex);
+            JOptionPane.showMessageDialog(null, "Error al conectarse a la base de datos", "ERROR DE CONEXIÓN DB", JOptionPane.ERROR_MESSAGE);
+            return null;
+        }
+    }
+    
+}
